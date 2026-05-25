c2369f68cd1774f1eb8db64f542be147c44db66f
6Tyson7 <trietquan1704@gmail.com>
2026-05-25 09:14:19 -0300

    Final Complete

--- MapApp/script.js
+++ MapApp/script.js
@@ -84,6 +84,70 @@ navigator.geolocation.getCurrentPosition(
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
+    const data = JSON.parse(localStorage.getItem("workouts"));
+
+    if (data) {
+      workouts = data;
+      console.log(data);
+
+      for (const workout of workouts) {
+        let lat = workout.coords[0];
+        let lng = workout.coords[1];
+        let html;
+
+        if (workout.type == "Running") {
+          html = ` <li class="workout workout--running" data-id=${workout.id}>
+                <h2 class="workout__title">${workout.description}</h2>
+                <div class="workout__details">
+                  <span class="workout__icon">🏃‍♂️</span>
+                  <span class="workout__value">${workout.distance}</span>
+                  <span class="workout__unit">km</span>
+                </div>
+                <div class="workout__details">
+                  <span class="workout__icon">⏱</span>
+                  <span class="workout__value">${workout.duration}</span>
+                  <span class="workout__unit">min</span>
+                </div>
+                <div class="workout__details">
+                  <span class="workout__icon">⚡️</span>
+                  <span class="workout__value">${workout.pace}</span>
+                  <span class="workout__unit">min/km</span>
+                </div>
+                <div class="workout__details">
+                  <span class="workout__icon">🦶🏼</span>
+                  <span class="workout__value">${workout.cadence}</span>
+                  <span class="workout__unit">spm</span>
+                </div>
+              </li>`;
+        } else if (workout.type == "Cycling") {
+          html = ` <li class="workout workout--cycling" data-id=${workout.id}>
+                  <h2 class="workout__title">Cycling on April 5</h2>
+                  <div class="workout__details">
+                    <span class="workout__icon">🚴‍♀️</span>
+                    <span class="workout__value">${workout.distance}</span>
+                    <span class="workout__unit">km</span>
+                  </div>
+                  <div class="workout__details">
+                    <span class="workout__icon">⏱</span>
+                    <span class="workout__value">${workout.duration}</span>
+                    <span class="workout__unit">min</span>
+                  </div>
+                  <div class="workout__details">
+                    <span class="workout__icon">⚡️</span>
+                    <span class="workout__value">${workout.speed}</span>
+                    <span class="workout__unit">km/h</span>
+                  </div>
+                  <div class="workout__details">
+                    <span class="workout__icon">⛰</span>
+                    <span class="workout__value">${workout.elevationGain}</span>
+                    <span class="workout__unit">m</span>
+                  </div>;`;
+        }
+
+        form.insertAdjacentHTML("afterend", html);
+      }
+    }
+
     L.marker([latitude, longtitude])
       .addTo(map)
       .bindPopup(`${latitude}, ${longtitude}`)
@@ -149,6 +213,8 @@ form.addEventListener("submit", function (e) {
 
   console.log(workouts);
 
+  localStorage.setItem("workouts", JSON.stringify(workouts));
+
   L.marker([lat, lng])
     .addTo(map)
     .bindPopup(
@@ -189,6 +255,21 @@ form.addEventListener("submit", function (e) {
             <span class="workout__unit">spm</span>
           </div>
         </li>`;
+
+    L.marker([lat, lng])
+      .addTo(map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+
+      .setPopupContent("Workout")
+      .openPopup();
   } else if (workout.type == "Cycling") {
     html = ` <li class="workout workout--cycling" data-id=${workout.id}>
           <h2 class="workout__title">Cycling on April 5</h2>
@@ -212,9 +293,39 @@ form.addEventListener("submit", function (e) {
             <span class="workout__value">${workout.elevationGain}</span>
             <span class="workout__unit">m</span>
           </div>;`;
+
+    L.marker([latitude, longtitude])
+      .addTo(map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: "cycling-popup",
+        })
+      )
+
+      .setPopupContent("Workout")
+      .openPopup();
   }
 
   form.insertAdjacentHTML("afterend", html);
 
   form.reset();
+
+  containerWorkouts.addEventListener("click", function (e) {
+    const workoutEl = e.target.closest(".workout");
+
+    if (!workoutEl) return;
+
+    const workout = workouts.find((work) => work.id === workoutEl.dataset.id);
+
+    map.setView(workout.coords, 13, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+  });
 });
